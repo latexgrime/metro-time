1baefcada572d0dee96bfe09d1e907582c5412b1
Dyson <dysonkui@gmail.com>
2025-02-14 15:47:58 +0400

    EnemyAI using State Machine

---
+++ Assets/!Scripts/Enemy/Trigger Checks/EnemyAggroCheck.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+public class EnemyAggroCheck : MonoBehaviour
+{
+    public GameObject PlayerTarget { get; set; }
+    private Enemy enemy;
+
+    private void Awake()
+    {
+        PlayerTarget = GameObject.FindGameObjectWithTag("PlayerTarget");
+        enemy = GetComponentInParent<Enemy>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject == PlayerTarget)
+        {
+            enemy.SetAggroStatus(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject == PlayerTarget)
+        {
+            enemy.SetAggroStatus(false);
+        }
+    }
+}
