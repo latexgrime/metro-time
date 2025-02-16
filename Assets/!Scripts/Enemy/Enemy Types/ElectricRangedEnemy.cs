d63735ba092b1825ad82d377ceaa1f201cdfdbb8
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2025-02-16 12:42:55 +0400

    Everything set for enemy animations.

--- Assets/!Scripts/Enemy/Enemy Types/ElectricRangedEnemy.cs
+++ Assets/!Scripts/Enemy/Enemy Types/ElectricRangedEnemy.cs
@@ -1,42 +1,56 @@
-using _Scripts.StatusSystem;
 using UnityEngine;
 
 namespace _Scripts.Enemy.Enemy_Types
 {
     public class ElectricRangedEnemy : BaseEnemy
     {
-        [Header("- Attack Settings")]
-        [SerializeField] private GameObject electricProjectilePrefab;
+        [Header("- Attack Settings")] [SerializeField]
+        private GameObject electricProjectilePrefab;
+
         [SerializeField] private Transform projectileSpawnPoint;
         [SerializeField] private float projectileSpeed = 20f;
         [SerializeField] private float attackCooldown = 2f;
         [SerializeField] private float projectileLifetime = 5f;
-        
+
         private float _nextAttackTime;
+
+        protected override void Start()
+        {
+            base.Start();
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            UpdateAnimator();
+        }
 
         protected override void Attack()
         {
             if (Time.time >= _nextAttackTime)
             {
-                // Face the player.
-                Vector3 directionToPlayer = (player.position - transform.position).normalized;
+                var directionToPlayer = (player.position - transform.position).normalized;
                 transform.forward = new Vector3(directionToPlayer.x, 0, directionToPlayer.z);
 
-                // Create projectile.
-                GameObject projectile = Instantiate(electricProjectilePrefab, 
-                    projectileSpawnPoint.position, 
+                // Trigger attack animation.
+                if (animator != null) animator.SetTrigger("shoot");
+
+                // Shoot projectile.
+                var projectile = Instantiate(electricProjectilePrefab,
+                    projectileSpawnPoint.position,
                     Quaternion.LookRotation(directionToPlayer));
 
-                // Set up projectile.
-                ElectricProjectile electricProjectile = projectile.GetComponent<ElectricProjectile>();
-                if (electricProjectile != null)
-                {
-                    electricProjectile.Initialize(projectileSpeed, projectileLifetime);
-                }
+                var electricProjectile = projectile.GetComponent<ElectricProjectile>();
+                if (electricProjectile != null) electricProjectile.Initialize(projectileSpeed, projectileLifetime);
 
-                // Set next attack time
                 _nextAttackTime = Time.time + attackCooldown;
             }
+        }
+
+        protected override void UpdateAnimator()
+        {
+            if (animator != null) animator.SetBool("isDeactivated", isDeactivated);
         }
     }
 }
